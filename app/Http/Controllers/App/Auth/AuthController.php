c42ddc7f442f03feb331ea41b27855dae08fbd52
adityahendrawan103 <49647244+adityahendrawan103@users.noreply.github.com>
2022-10-21 14:40:20 +0700

    Test Upload
    Test Upload

--- app/Http/Controllers/App/Auth/AuthController.php
+++ app/Http/Controllers/App/Auth/AuthController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller {
 
     public function index() {
+        dd('tes');
         return view('layouts.auth.login');
     }
 
