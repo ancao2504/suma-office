64385cc1b43d92a43bfa5fe45203e785df0902b1
adityahendrawan103 <49647244+adityahendrawan103@users.noreply.github.com>
2023-02-25 11:42:24 +0700

    Update Constant
    Update constant

--- config/constants.php
+++ config/constants.php
@@ -3,7 +3,7 @@
 return [
     'app' => [
         'app_base_url'  => 'https://suma-honda.id/',
-        'app_api_url'   => 'http://localhost:8888/suma-pmo-api/public/api',
+        'app_api_url'   => 'http://43.252.9.117/suma-pmo-api/public/api',
         'app_images_url' => 'http://43.252.9.117/suma-images',
         // 'app_images_url_SJ' => 'http://192.168.1.47:2022/suma-pmo/public/assets/images/sj/',
     ],
