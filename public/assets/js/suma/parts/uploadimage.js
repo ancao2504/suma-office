021db5142116e8ec0ff8189a11b7e633fd22324f
Halim <abdulhalimmuklis@gmail.com>
2023-03-02 09:42:55 +0700

    update image upload

--- public/assets/js/suma/parts/uploadimage.js
+++ public/assets/js/suma/parts/uploadimage.js
@@ -1,18 +1,16 @@
 let fileList = new DataTransfer();
 let urut = 0;
-let page = 1;
+let page = 2;
+let scroll = true;
 
 function dropHandler(ev) {
     ev.preventDefault();
-    console.log('File(s) dropped');
     $('.dropzone').removeClass('p-10');
     uploadFile(ev.dataTransfer.files);
-    // console.log('trigger drop');
 }
 
 function dragOverHandler(ev) {
     ev.preventDefault();
-    // console.log('File(s) in drop zone');
     // drop zone effect
     $('.dropzone').addClass('p-10');
 }
@@ -42,7 +40,6 @@ function uploadFile(files) {
                     let file = new File([files[i]], files[i].name, {type: files[i].type});
                     fileList.items.add(file);
                     fileList.files[urut].urut = urut;
-                    console.log(fileList.files);
                     removeFile(urut);
                     urut++;
                 } else {
@@ -61,13 +58,11 @@ function uploadFile(files) {
 
 function removeFile(e){
     $('.urut-'+e).on('click', function(){
-        // looping fileList.files cari urut yang sama dengan e
         for (let i = 0; i < fileList.files.length; i++) {
             if(fileList.files[i].urut == e){
                 fileList.items.remove(i);
             }
         }
-        console.log(fileList.files);
         $(this).parent().remove();
         if ($('.dz-preview').length == 0) {
             $('.dropzone').removeClass('dz-started');
@@ -77,8 +72,6 @@ function removeFile(e){
 }
 
 function pilihGambar(url){
-
-    // aksess url terlebihdahulu jika tidak ditemukan maka jangan tampilkan
     var http = new XMLHttpRequest();
     http.open('HEAD', url, false);
     http.send();
@@ -89,61 +82,38 @@ function pilihGambar(url){
     }
 }
 
-
-function listGambar(page){
+function listGambar(){
+    console.log(page);
     $.ajax({
-        url: base_url + "/parts/uploadimage/part",
-        type: "POST",
+        url: window.location.href,
+        type: "GET",
         data: {
-            _token: $('input[name="_token"]').val(),
+            search: $('#inputCariPartNumber').val(),
             page: page
         },
         dataType: "json",
         success: function(data){
-            // console.log(data.data);
-            if (data.length != 0 || data != null) {
-                // looping data.data
-                for (let i = 0; i < data.data.length; i++) {
-                    var img = new Image();
-                    // untuk cek image apakah ada atau tidak, jika tidak ada akan di hendle oleh eventlistener error dan load
-                    img.src = data.data[i].url.trim();
-                    img.addEventListener('load', function() {
-                        $('#list-gambar').append(`
-                            <div class="col-lg-2 col-md-3 col-sm-4 col-lg-3 py-3" onclick="pilihGambar('${data.data[i].url.trim()}')" style="cursor: pointer;">
-                                <div class="card border border-dark rounded">
-                                    <div class="d-flex justify-content-center">
-                                        <div class="bg-image rounded" style="background-image: url('${data.data[i].url.trim()}'); width: 100%; height: 200px; background-size: cover; background-position: center; background-repeat: no-repeat;">
-                                            <div class="bg-dark" style="width: 100%; height: 50px; position: absolute; bottom: 0; opacity: 0.8;">
-                                                <div class="d-flex justify-content-center align-items-center" style="height: 100%;">
-                                                    <span class="text-white">${data.data[i].kd_part}</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        `);
-                    });
-                    img.addEventListener('error', function(){
-                        $('#list-gambar').append(`
-                            <div class="col-lg-2 col-md-3 col-sm-4 col-lg-3 py-3">
-                                <div class="card border border-dark rounded">
-                                    <div class="d-flex justify-content-center">
-                                        <div class="bg-image rounded" style="background-image: url('http://localhost:2022/suma-pmo/public/assets/images/default.png'); width: 100%; height: 200px; background-size: cover; background-position: center; background-repeat: no-repeat;">
-                                            <div class="bg-dark" style="width: 100%; height: 50px; position: absolute; bottom: 0; opacity: 0.8;">
-                                                <div class="d-flex justify-content-center align-items-center" style="height: 100%;">
-                                                    <span class="text-white">${data.data[i].kd_part}</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        `);
-                    });
-                }
+            console.log(data);
+            if (data != null && data.status == 1) {
+                $('#list-gambar').append(data.data);
+                $('#spin-page').remove();
+                page++;
+            } else if (data != null && data.status == 0) {
+                $('#list-gambar').append(`
+                    <div class="col-12 text-center">
+                        <span class="text-danger">${data.message}</span>
+                    </div>
+                `);
+                scroll = false;
+                $('#spin-page').remove();
+            } else if (data != null && data.status == 2) {
+                scroll = false;
                 $('#spin-page').remove();
             }
+        },
+        error: function(data){
+            scroll = false;
+            console.log(data);
         }
     });
 }
@@ -153,7 +123,44 @@ $(document).ready(function(){
         loading.block();
     });
 
-    // button submit klik maka form akan di submit
+    $(document).ajaxStart(function () {
+        loading.block();
+    });
+
+    $(document).ajaxStop(function () {
+        loading.release();
+    });
+
+    $('#inputCariPartNumber').on('change', function(){
+        $('#list-gambar').html('');
+        page = 1;
+        scroll = true;
+        listGambar();
+    });
+    $('#cariImagePart').on('click', function(){
+        $('#list-gambar').html('');
+        page = 1;
+        scroll = true;
+        listGambar();
+    });
+
+    $(window).scroll(function(){
+        if(($(window).scrollTop() + $(window).height()) == $(document).height() && (scroll == true || $('#inputCariPartNumber').val() == '')) {
+            if ($('#spin-page').length == 0) {
+                $('#list-gambar').append(`
+                    <div class="col-lg-12 col-md-12 col-sm-12 py-3" id="spin-page">
+                        <div class="d-flex justify-content-center">
+                            <div class="spinner-border text-primary" role="status">
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                        </div>
+                    </div>
+                `);
+                listGambar(page);
+            }
+        }
+    });
+
     $('#btnSubmit').on('click', function(){
         if ($('.dz-preview').length != 0) {
             swal.fire({
@@ -171,7 +178,7 @@ $(document).ready(function(){
             }).then((result) => {
                 if (result.isConfirmed) {
                     $('input[name="file[]"]').prop('files', fileList.files);
-                    // jika prop diatas sudah selesai diproses maka form akan di submit
+
                     if ($('input[name="file[]"]').prop('files').length == fileList.files.length) {
                         $('#drop_zone').submit();
                     }
@@ -183,13 +190,11 @@ $(document).ready(function(){
     });
 
     
-    // jika dropzone tidak terjadi maka akan dijalankan
     $('.dropzone').on('dragleave', function(){
         // $('.dropzone').removeClass('p-10');
     });
 
     $('.dropzone').on('click', function(e) {
-        // kecuali ynag aca class .dz-message, .dz-preview, .dz-remove
         if (!$(e.target).is('.dz-remove, .dz-image, .dz-details, .dz-details.dz-filename, .dz-details.dz-size, .dz-details.dz-filename span, .dz-details.dz-size span')) {
             $('#file').trigger('click');
         }
@@ -197,29 +202,4 @@ $(document).ready(function(){
     $('#file').on('change', function(){
         uploadFile(this.files);
     });
-    
-    // let imgindex = 0;
-    $(window).scroll(function(){
-        if($(window).scrollTop() + $(window).height() == $(document).height()) {
-            // jika #spin-page tidak ada maka akan di append
-            if ($('#spin-page').length == 0) {
-                $('#list-gambar').append(`
-                    <div class="col-lg-12 col-md-12 col-sm-12 py-3" id="spin-page">
-                        <div class="d-flex justify-content-center">
-                            <div class="spinner-border text-primary" role="status">
-                                <span class="sr-only">Loading...</span>
-                            </div>
-                        </div>
-                    </div>
-                `);
-                setTimeout(function(){
-                    listGambar(page);
-                }, 1000);
-                page++;
-            }
-        }
-    });
-
-    
-    listGambar(page);
 });
