fb2fd2813e35394306f07e45d9b5651c75c3e194
Halim <abdulhalimmuklis@gmail.com>
2023-03-08 16:25:43 +0700

    update

---
+++ app/Http/Controllers/App/Online/ApiProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\app\Online;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiProductController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+}
