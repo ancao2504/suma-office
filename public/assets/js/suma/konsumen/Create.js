0ec5ce2ecc763e8553b40f67a279e12315d03e6a
Halim <72723049+ABDUL-HALIM-MUKLIS@users.noreply.github.com>
2023-08-15 08:34:33 +0700

    Update Create.js

--- public/assets/js/suma/konsumen/Create.js
+++ public/assets/js/suma/konsumen/Create.js
@@ -331,6 +331,16 @@ $(document).ready(function () {
             cekfaktur();
         }
     });
+    $('#company').on('change', function () {
+        $('#lokasi').html('');
+        for (const [key, item] of Object.entries(company[$('input[name="divisi"]:checked').val()].lokasi)) {
+            if(item.companyid == $('#company').val()){
+                item.kd_lokasi.forEach(item => {
+                    $('#lokasi').append(`<option value="${item}">${item}</option>`);
+                });
+            }
+        };
+    });
 
     $("#inputTanggalLahir").flatpickr({
         dateFormat: "d-m-Y",
