84333eaed93b87890e87b0dcf34af0f1bc7583f1
Halim <72723049+ABDUL-HALIM-MUKLIS@users.noreply.github.com>
2023-08-23 19:47:24 +0700

    update

--- app/Http/Controllers/App/Home/HomeController.php
+++ app/Http/Controllers/App/Home/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     public function index(Request $request) {
         if(Str::contains(strtoupper(trim($request->session()->get('app_user_role_id'))), 'OL')) {
-            if(in_array($request->session()->get('app_user_role_id'), ['MD_OL_ADMIN'])){
+            if(in_array($request->session()->get('app_user_role_id'), ['MD_OL_ADMIN','MD_OL_SPV'])){
                 return view('layouts.home.homeonline', [
                     'title_menu'    => 'Home',
                 ]);
--- resources/views/layouts/main/index.blade.php
+++ resources/views/layouts/main/index.blade.php
@@ -168,7 +168,6 @@
         <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
-
 		<!--begin::Javascript-->
 		<script>
             const base_url = "{{ url('/') }}";
