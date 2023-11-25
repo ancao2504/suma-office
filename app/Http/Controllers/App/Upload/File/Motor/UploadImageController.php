d4565d5dfb15a5bdf5f4e03af594425d61b827c1
Halim <72723049+ABDUL-HALIM-MUKLIS@users.noreply.github.com>
2023-11-25 11:10:16 +0700

    Update UploadImageController.php

--- app/Http/Controllers/App/Upload/File/Motor/UploadImageController.php
+++ app/Http/Controllers/App/Upload/File/Motor/UploadImageController.php
@@ -55,10 +55,10 @@ class UploadImageController extends Controller
                 'gambar' => []
             ];
             foreach ($request->m_logo as $key => $value) {
-                $path->logo[] = $value->getClientOriginalName();
+                $path->logo[] = asset($value->getClientOriginalName());
             }
             foreach ($request->m_gambar as $key => $value) {
-                $path->gambar[] = $value->getClientOriginalName();
+                $path->gambar[] = asset($value->getClientOriginalName());
             }
 
             foreach ($request->m_logo as $key => $value) {
@@ -148,7 +148,7 @@ class UploadImageController extends Controller
             ];
 
             foreach ($request->di_gambar as $key => $value) {
-                $path->gambar[] = $value->getClientOriginalName();
+                $path->gambar[] = asset($value->getClientOriginalName());
             }
             foreach ($request->di_gambar as $key => $value) {
                 $value->move('images/upload/motor', $value->getClientOriginalName());
